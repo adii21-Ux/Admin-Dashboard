787d297181c8e9be42498badcb06a6ae40f74d06
adii21-ux <adityasangave2112@gmail.com>
2023-12-03 02:19:06 +0530

    React Router Setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './Components/Main';
 
 function App() {
   return (
     <div className="App">
-      <h1>Admin Dashboard</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
